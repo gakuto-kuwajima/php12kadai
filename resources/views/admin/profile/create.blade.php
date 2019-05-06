59bc7795d36ac2d7eba2428aba4cf8a06c1dd303
gakuto <mydance365@gmail.com>
2019-05-07 01:41:10 +0900

    2019-05-06 php12kadai

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,13 +1,15 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+{{-- layouts/profile.blade.phpを読み込む --}}
+@extends('layouts.profile')
 
-        <title>プロフィール作成画面</title>
-    </head>
-    <body>
-        <h1>プロフィール作成画面</h1>
-    </body>
-</html>
+{{-- profile.blade.phpの@yield('title')に'ニュース新規作成'を埋め込む --)}}
+@section('title', 'プロフィールページ')
+
+{{-- profile.blade.phpの@yield'('content')に以下のタグを埋め込む --}}
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>Myプロフィール</h2>
+            </div>
+        </div>
+@endsection
